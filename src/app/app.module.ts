5a3dfbcd89658a035b62eb53d5c29267588214b4
Avital Koren <avitalar1@gmail.com>
2018-10-29 11:54:42 +0200

    complete course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { GitSearchService } from './git-search.service';
 import { GitSearchComponent } from './git-search/git-search.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-
 const appRoutes: Routes = [
   { path: '', 
     component: HomePageComponent 
   },
   { path: 'search',      
+    redirectTo: '/search/angular', 
+    pathMatch: 'full' },
+  {
+    path: 'search/:query',
     component: GitSearchComponent,
-    data: { 
-      title: 'Git Search' 
-    } 
+    data: {title: 'Git Search'}
   },
   { path: '**', component: NotFoundComponent }
 ];
